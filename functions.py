585c15fe5fd45bb8443d7edcb77a186ef468649c
RahulS416 <144241896+RahulS416@users.noreply.github.com>
2023-10-19 13:13:09 +0000

    edit

--- functions.py
+++ functions.py
@@ -2,16 +2,17 @@
 
 # Returns a string that is the contents of the myName variable written backwards
 # ie: passing Atkinson as the parameter would return nosniktA
+import math
 def reverseName(myName):
   # Write your code here
-  
-  result = myName
+  result = ''
+  for i in range(len(myName)):
+    result += myName[-1 * (i+1)]
   return result
 
 # Returns a float value that is the contents of the myAge variable square rooted
 # ie: passing 16 as the parameter would return 4.0
 def rootAge(myAge):
   # Wrie your code here
-  
-  result = myAge
+  result = math.sqrt(int(myAge))
   return result
